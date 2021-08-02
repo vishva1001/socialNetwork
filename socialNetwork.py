f8f5575d01cb6c25bea8e5f615dc8123e8f44670
vishva1001 <54721740+vishva1001@users.noreply.github.com>
2021-08-02 15:35:11 -0400

    clean, consolidate and load data to graph database

--- socialNetwork.py
+++ socialNetwork.py
@@ -1,76 +1,45 @@
 import neo4j
 from neo4j import GraphDatabase, basic_auth
 import pandas as pd
+import os
 
-#connect to neo4j sandbox
-#driver = GraphDatabase.driver("bolt://3.238.152.123:7687", auth=basic_auth("neo4j", "striker-twists-bearing"))
-driver = GraphDatabase.driver("neo4j+s://8c36a61c.databases.neo4j.io", auth=basic_auth("neo4j","SHcCzmlwObYs_BgJyxVcvT3KkyeA0A1LBkXW8Kt7z7A"))
-#query
-cypher_query = '''
-MATCH (movie:Movie)<-[:DIRECTED]-(p:Person {name:'Nora Ephron'})
- RETURN movie.title as title'''
+#connect to neo4j aura
+driver = GraphDatabase.driver(os.environ['NEO4J_DB'], auth=basic_auth(os.environ['NEO4J_USR'],['NEO4J_PWD']))
 
-#new session
-'''session = driver.session()
-results = session.run(cypher_query)
-for record in results:
-  print(record['title'])
-session.close()
-#session closed
-'''
-
-#trim spaces
+#trim spaces from dataset
 def trim(dataset):
     trim = lambda x: x.strip() if type(x) is str else x
     return dataset.applymap(trim)
+
+eptxt="/home/ubuntu/static/entity_properties.txt"
+ertxt="/home/ubuntu/static/entity_relationships.txt"
+epcsv="/home/ubuntu/static/entity_properties.csv"
+ercsv="/home/ubuntu/static/entity_relationships.csv"
+
 #text to csv
-read_file = trim(pd.read_csv('/home/ubuntu/static/entity_properties.txt', delimiter='\t', skipinitialspace = True ))
-read_file.to_csv('/home/ubuntu/static/entity_properties.csv', index=False)
-read_file2 = trim(pd.read_csv('/home/ubuntu/static/entity_relationships.txt', delimiter='\t', skipinitialspace = True))
-read_file2.to_csv('/home/ubuntu/static/entity_relationships.csv', index=False)
+read_file = trim(pd.read_csv(eptxt, delimiter='\t', skipinitialspace = True ))
+read_file.to_csv(epcsv, index=False)
+read_file2 = trim(pd.read_csv(ertxt, delimiter='\t', skipinitialspace = True))
+read_file2.to_csv(ercsv, index=False)
 
-'''#trim extra spaces
-with open("entity_properties.csv") as infile:
-    reader=csv.DictReader(infile)
-    fieldnames=reader.fieldnames
-    with open("xyz.csv", "w") as wr:
-        writer = csv.DictWriter(wr, fieldnames=fieldnames)
-        for row in reader:
-            row.update({fieldname: value.strip() for (fieldname, value) in row.items()})
-            print({fieldname: value.strip() for (fieldname, value) in row.items()})
-            writer.writerow(row)
-'''
-#new session
+#new database session
 session=driver.session()
+#clear any existing data
 session.run('match (n) detach delete (n)')
-df = pd.read_csv('/home/ubuntu/static/entity_properties.csv')
-header=list(df.columns)
-#Enter entities and properties
-for ind in df.index:
-    result=session.run("MERGE (a {"+header[0]+":'"+df[header[0]][ind]+"'}) on match set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"' on create set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"'")
-'''
-for ind in df.index:
-    if df[header[1]][ind]=="Phone":
-        result = session.run("MATCH (n:Person {ID:'"+df[header[0]][ind]+"'}) return n.Phone as Phone")
-        for record in result:
-            print(record)
-            if record['Phone'] is None:
-                #print("do this")
-                result=session.run("MERGE (a {"+header[0]+":'"+df[header[0]][ind]+"'}) on match set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"' on create set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"'")
-            else:
-                result=session.run("MERGE (a {"+header[0]+":'"+df[header[0]][ind]+"'}) on match set a."+df[header[1]][ind]+"=[a.Phone,'"+df[header[2]][ind]+"'] on create set a."+df[header[1]][ind]+"=[a.Phone,'"+df[header[2]][ind]+"']")
-    else:
-        result=session.run("MERGE (a {"+header[0]+":'"+df[header[0]][ind]+"'}) on match set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"' on create set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"'")
-'''
 
-#Enter label and relationships
-df = pd.read_csv('/home/ubuntu/static/entity_relationships.csv')
+#Load entities and properties
+df = pd.read_csv(epcsv)
 header=list(df.columns)
 for ind in df.index:
-    result=session.run("MATCH (n {ID:'"+df[header[1]][ind]+"'}) set n:"+df[header[2]][ind])
-    result=session.run("MATCH (n {ID:'"+df[header[3]][ind]+"'}) set n:"+df[header[4]][ind])
-    result=session.run("MATCH (a {ID:'"+df[header[1]][ind]+"'}), (b {ID:'"+df[header[3]][ind]+"'}) create (a)-[r:"+df[header[0]][ind]+"]->(b) return(type(r))")
-    #if df[header[3]][ind]=='Person' and df[header[5]][ind]=='Person':
-        #result=session.run("MATCH (a {ID:'"+df[header[1]][ind]+"'}), (b {ID:'"+df[header[3]][ind]+"'}) create (b)-[r:"+df[header[0]][ind]+"]->(a) return(type(r))")
+    session.run("MERGE (a {"+header[0]+":'"+df[header[0]][ind]+"'}) on match set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"' on create set a."+df[header[1]][ind]+"='"+df[header[2]][ind]+"'")
+
+#Load label and relationships
+df = pd.read_csv(ercsv)
+header=list(df.columns)
+for ind in df.index:
+    session.run("MATCH (n {ID:'"+df[header[1]][ind]+"'}) set n:"+df[header[2]][ind])
+    session.run("MATCH (n {ID:'"+df[header[3]][ind]+"'}) set n:"+df[header[4]][ind])
+    session.run("MATCH (a {ID:'"+df[header[1]][ind]+"'}), (b {ID:'"+df[header[3]][ind]+"'}) create (a)-[r:"+df[header[0]][ind]+"]->(b) return(type(r))")
+
 session.close()
 driver.close()
